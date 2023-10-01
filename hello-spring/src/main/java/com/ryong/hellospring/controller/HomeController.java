a8a96811961c6a08ac7f41ca0188777c0120fab4
장청룡 <wkdcjdfyd@naver.com>
2023-10-01 20:27:02 +0900

    Create member func

---
+++ hello-spring/src/main/java/com/ryong/hellospring/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.ryong.hellospring.controller;
+
+import com.ryong.hellospring.domain.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+}
---
+++ hello-spring/src/main/java/com/ryong/hellospring/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package com.ryong.hellospring.controller;
+
+import javax.swing.*;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
