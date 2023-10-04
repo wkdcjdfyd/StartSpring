dfe35b235659a5d611f412ec2cd6b55ccc901604
장청룡 <wkdcjdyfd@naver.com>
2023-10-05 08:06:17 +0900

    Create JpaMemberRepository

--- hello-spring/src/main/java/com/ryong/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/ryong/hellospring/SpringConfig.java
@@ -1,23 +1,31 @@
 package com.ryong.hellospring;
 
 import com.ryong.hellospring.repository.JDBCTemplateMemberRepository;
+import com.ryong.hellospring.repository.JpaMemberRepository;
 import com.ryong.hellospring.repository.MemberRepository;
 import com.ryong.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource){
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource){
+//        this.dataSource = dataSource;
+//    }
 
     @Bean
     public MemberService memberService(){
@@ -28,6 +36,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JDBCMemberRepository(dataSource);
-        return new JDBCTemplateMemberRepository(dataSource);
+        //return new JDBCTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
