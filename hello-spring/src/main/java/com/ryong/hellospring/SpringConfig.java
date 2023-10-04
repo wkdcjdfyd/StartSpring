27f491f0e2e31d090d4e300641365c97e8db57d7
장청룡 <wkdcjdyfd@naver.com>
2023-10-05 08:39:24 +0900

    Create SpringDataJpaMemberRepository

--- hello-spring/src/main/java/com/ryong/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/ryong/hellospring/SpringConfig.java
@@ -14,12 +14,19 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 //    private DataSource dataSource;
 //
 //    @Autowired
@@ -29,14 +36,14 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+    /*@Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JDBCMemberRepository(dataSource);
         //return new JDBCTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+    }*/
 }
